23bd2240fad16f7c1b6dc6982531aab730923c6f
Tobias Mattsson <tobias.mattsson@gmail.com>
2018-11-12 06:23:22 +0100

    adds crapper scaliing wip

--- conf.lua
+++ conf.lua
@@ -1,6 +1,5 @@
 function love.conf(t)
 	t.title = "Crapper"
 	t.version = "11.1"
-	t.window.width = 320
-	t.window.height = 180
+	t.console = true
 end
--- main.lua
+++ main.lua
@@ -1,13 +1,32 @@
-debug = true
+local desktopResX, desktopResY = love.window.getDesktopDimensions()
+local localResX = 320
+local localResY = 180
+local integerX = math.floor(desktopResX / localResX)
+local integerY = math.floor(desktopResY / localResY)
+
+local desiredScale = integerX < integerY and integerX or integerY
+local scaledResX = localResX * desiredScale
+local scaledResY = localResY * desiredScale
+
+local offsetX = (desktopResX - scaledResX) / 2
+local offsetY = (desktopResY - scaledResY) / 2
 
 function love.load(arg)
-    
+	love.window.setFullscreen(true)
+    love.graphics.setDefaultFilter('nearest', 'nearest', 0)
+	playerImg = love.graphics.newImage('assets/lonk-1.png')
 end
 
 function love.update(dt)
-
 end
 
 function love.draw(dt)
-    love.graphics.print("Hello World", 2, 2)
+	love.graphics.setScissor(offsetX, offsetY, offsetX + scaledResX, offsetY + scaledResY)
+	love.graphics.translate(offsetX, offsetY)
+	love.graphics.scale(desiredScale, desiredScale)
+
+	love.graphics.setBackgroundColor(.2, .2, .2)
+    love.graphics.draw(playerImg, 1, 1)
+    love.graphics.draw(playerImg, 100, 100)
+    love.graphics.draw(playerImg, 288 - 16, 180 - 16)
 end
